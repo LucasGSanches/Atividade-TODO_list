f96454f070be30b49bc7a47e58fb757b776abd95
Lucas Sanches <lucasgarciasanchesmuniz@gmail.com>
2026-03-26 14:39:04 -0300

    activity finished

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Atividade-TODO_list
-Atividade universitária da disciplina de desenvolvimento web sobre html, css e javascript
+Atividade universitária da disciplina de desenvolvimento web sobre html, css e javascript.
+
+**Descrição breve do projeto**
+
+O projeto utiliza de html, css e javascript, bem como algumas boas práticas como css reset e html semântico afim de 
+fazer um site sobre "todo-list"(lista de afazeres).
+
+**Instruções de como executar a aplicação**
+
+Para utilizar o site, o usuário deve selecionar a entrada de texto para colocar a descrição da atividade,
+e depois apertar no botão "add" para adicionar a tarefa na lista. Para continuar adicionando tarefas basta
+repetir os passos anteriores, e caso deseja remover alguma tarefa, basta clicar no "X" na tarefa desejada.
--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+const container = document.querySelector(".container");
+const text_input = document.querySelector("input");
+const button = document.querySelector("button");
 
+button.addEventListener('click', function(){
+    let text = text_input.value;
+    if(text == ""){
+        return;
+    }
+    let todo_element = document.createElement("div");
+    let todo_text = document.createElement("p");
+    let todo_button = document.createElement("button");
+    let text_node = document.createTextNode(text);
+    let icon_node = document.createTextNode("\u2718");
+
+    todo_button.addEventListener('click', function(){
+        todo_element.remove();
+    })
+
+    todo_text.appendChild(text_node);
+    todo_button.appendChild(icon_node);
+    todo_element.appendChild(todo_text);
+    todo_element.appendChild(todo_button);
+    todo_element.classList.add("todo_element");
+    container.appendChild(todo_element);
+    text_input.value = "";
+})
